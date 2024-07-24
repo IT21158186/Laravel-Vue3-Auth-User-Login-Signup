4e4c27b30c512ffa30192bfc8c94e76fdc51887f
IT21158186 <it21158186@my.sliit.lk>
2024-07-24 16:06:08 +0530

    implement frontend files/pages

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ env('APP_Name') }}</title>
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
 
 </head>
 
 <body>
+    @if (Auth::check())
+        <script>
+            window.Laravel = {
+                !!json_encode([
+                    'isLoggedIn' => true,
+                    'user' => Auth::user(),
+                ])
+            }
+        </script>
+    @else
+        <script>
+            window.Laravel = {
+                !!json_encode([
+                    'isLoggedIn' => false,
+                ])
+            }
+        </script>
+    @endif
+
+
     <div id="app">
         <!-- Your Vue.js App -->
     </div>
